3fd6cf8b5c619996d7b145f4cb6e70965ee03260
James Dishman <nomadjames@msn.com>
2026-02-16 10:01:47 -0500

    Initial Commit

---
+++ SensorSynthFM/SensorSynthFMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensorSynthFMApp.swift
+//  SensorSynthFM
+//
+//  Created by nomad james on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SensorSynthFMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SensorSynthFMTests/SensorSynthFMTests.swift
@@ -0,0 +1,17 @@
+//
+//  SensorSynthFMTests.swift
+//  SensorSynthFMTests
+//
+//  Created by nomad james on 2/16/26.
+//
+
+import Testing
+@testable import SensorSynthFM
+
+struct SensorSynthFMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
